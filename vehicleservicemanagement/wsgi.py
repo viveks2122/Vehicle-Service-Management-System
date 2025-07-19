eaad77b2bc6887e9cd29f450508367b5424fde41
vivek <vivekvkbca01@gmail.com>
2025-07-19 09:24:27 +0530

    Updated settings and prepared for Render deploy

--- vehicleservicemanagement/wsgi.py
+++ vehicleservicemanagement/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
 
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicleservicemanagement.settings')
