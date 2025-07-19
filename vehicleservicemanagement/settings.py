65c99fc7bdb202708e7339c99f830b0747a747ea
vivek <vivekvkbca01@gmail.com>
2025-07-19 12:13:03 +0530

    Fixed DATABASES setup with dj_database_url

--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -12,6 +12,26 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
+
+
+
+DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
